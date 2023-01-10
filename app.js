331ef627291c993420dd27f2fec7d42cc2f77fbf
mohamad <mohamad.dacca@gmail.com>
2023-01-10 06:37:02 +0200

    Added a forgot password option

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // const path=require("path");
- const signupDoctor=require("./mongodb");
- const signupNurse=require("./mongodb");
- const signupPatient=require("./mongodb");
+const signupDoctor=require("./mongodb");
+const signupNurse=require("./mongodb");
+const signupPatient=require("./mongodb");
 //  const jsdom=require("jsdom");
 //  const { JSDOM } = jsdom;
 // const templatePath=path.join(__dirname,"../views");
@@ -103,19 +103,80 @@ app.post("/signupNurse",async  function(req,res){
     });
 
     app.post("/login",async function(req,res){
-        try{
-            const check= await signupDoctor.signUpDoctor.findOne({username:req.body.userName})
-            if(check.password===req.body.password){
+            let check1,check2,check3;
+            check1 = await signupDoctor.signUpDoctor.findOne({ username: req.body.userName, password: req.body.password });
+            check2 = await signupNurse.signupNurse.findOne({ username: req.body.userName, password: req.body.password });
+            check3 = await signupPatient.signupPatient.findOne({ username: req.body.userName, password: req.body.password });
+            // If a matching document was found, render the "home" page
+            if (check1!=null && check2===null &&check3===null) {
+                res.render("home");
+            }else if(check1===null && check2!=null &&check3===null){
+                res.render("home");
+            }else if(check1===null && check2===null &&check3!=null){
                 res.render("home");
             }else{
-                res.send("wrong password");
+                res.send("Wrong username/password");
             }
-        }
-        catch{
-            res.send("wrong detials");
-        }
-    });
+});
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
-  });
+    app.get("/forgetpassword",function(req,res){
+        res.render("forgetpassword");
+    })
+
+app.post("/forgetpassword", function(req, res) {
+    if (req.body.code === "6578") {
+        signupDoctor.signUpDoctor.findOne({ username: req.body.userName }, function(err,user) {
+            if(!err){
+                res.send("invalid username");
+            }else{
+                user.password = req.body.password;
+                user.confirmPassword=req.body.password;
+                user.save(function(err){
+                    if(err){
+                        res.send("Error updating password");
+                    }else{
+                        res.render("login");
+                    }
+                });
+            }
+        });
+        }else if(req.body.code === "8756"){
+            signupPatient.signupPatient.findOne({username:req.body.userName},function(err,user){
+                if(!err){
+                    res.send("invalid username");
+                }else{
+                    user.password = req.body.password;
+                    user.confirmPassword=req.body.password;
+                    user.save(function(err){
+                        if(err){
+                            res.send("Error updating password");
+                        }else{
+                            res.render("login");
+                        }
+                    })
+                }
+            });
+        } else if(req.body.code === "9856"){
+            signupPatient.signupPatient.findOne({username:req.body.userName},function(err,user){
+                if(!err){
+                    res.send("invalid username");
+                }else{
+                    user.password = req.body.password;
+                    user.confirmPassword=req.body.password;
+                    user.save(function(err){
+                        if(err){
+                            res.send("Error updating password");
+                        }else{
+                            res.render("login");
+                        }
+                    })
+                }
+            });
+        }else{
+            res.send("wrong validation code !");
+        }
+});
+
+app.listen(3824, function() {
+    console.log("Server started on port 3824");
+});
